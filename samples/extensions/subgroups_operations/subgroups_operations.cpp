05ac1aed536f1804f79e457c4c7388941623324d
Patryk-Jastrzebski-Mobica <patryk.jastrzebski@mobica.com>
2023-04-20 10:45:54 +0200

    Add subgroups_operations sample

---
+++ samples/extensions/subgroups_operations/subgroups_operations.cpp
@@ -0,0 +1,400 @@
+/* Copyright (c) 2023, Mobica Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "subgroups_operations.h"
+
+SubgroupsOperations::SubgroupsOperations()
+{
+	title       = "Subgroups operations";
+	camera.type = vkb::CameraType::LookAt;
+
+	camera.set_perspective(60.0f, static_cast<float>(width) / static_cast<float>(height), 256.0f, 0.1f);
+}
+
+SubgroupsOperations::~SubgroupsOperations()
+{
+	if (device)
+	{
+		vkDestroyPipeline(get_device().get_handle(), texturePipeline, nullptr);
+
+		vkDestroyPipelineLayout(get_device().get_handle(), pipeline_layout, nullptr);
+		vkDestroyDescriptorSetLayout(get_device().get_handle(), descriptor_set_layout, nullptr);
+		vkDestroySampler(get_device().get_handle(), texture.sampler, nullptr);
+	}
+
+	vertex_buffer.reset();
+	index_buffer.reset();
+	uniform_buffer_vs.reset();
+}
+
+bool SubgroupsOperations::prepare(vkb::Platform &platform)
+{
+	if (!ApiVulkanSample::prepare(platform))
+	{
+		return false;
+	}
+
+	load_assets();
+	generate_quad();
+	prepare_uniform_buffers();
+	setup_descriptor_set_layout();
+	setup_pipelines();
+	setup_descriptor_pool();
+	setup_descriptor_set();
+	create_command_buffers();
+	build_command_buffers();
+
+	prepared = true;
+	return true;
+}
+
+void SubgroupsOperations::request_gpu_features(vkb::PhysicalDevice &gpu)
+{
+	// auto &reqGpu = gpu.request_extension_features
+	if (gpu.get_features().samplerAnisotropy)
+	{
+		gpu.get_mutable_requested_features().samplerAnisotropy = VK_TRUE;
+	}
+}
+
+void SubgroupsOperations::prepare_uniform_buffers()
+{
+	uniform_buffer_vs = std::make_unique<vkb::core::Buffer>(get_device(),
+	                                                        sizeof(ubo_vs),
+	                                                        VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+	                                                        VMA_MEMORY_USAGE_CPU_TO_GPU);
+
+	update_uniform_buffers();
+}
+
+void SubgroupsOperations::update_uniform_buffers()
+{
+	ubo_vs.model      = glm::mat4(1.0f);
+	ubo_vs.model      = glm::translate(ubo_vs.model, glm::vec3(1.0f));
+	ubo_vs.view       = camera.matrices.view;
+	ubo_vs.projection = camera.matrices.perspective;
+
+	uniform_buffer_vs->convert_and_update(ubo_vs);
+}
+
+void SubgroupsOperations::setup_pipelines()
+{
+	VkPipelineInputAssemblyStateCreateInfo input_assembly_state =
+	    vkb::initializers::pipeline_input_assembly_state_create_info(
+	        VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+	        0,
+	        VK_FALSE);
+
+	VkPipelineRasterizationStateCreateInfo rasterization_state =
+	    vkb::initializers::pipeline_rasterization_state_create_info(
+	        VK_POLYGON_MODE_FILL,
+	        VK_CULL_MODE_NONE,
+	        VK_FRONT_FACE_COUNTER_CLOCKWISE,
+	        0);
+
+	VkPipelineColorBlendAttachmentState blend_attachment_state =
+	    vkb::initializers::pipeline_color_blend_attachment_state(
+	        0xf,
+	        VK_FALSE);
+
+	VkPipelineColorBlendStateCreateInfo color_blend_state =
+	    vkb::initializers::pipeline_color_blend_state_create_info(
+	        1,
+	        &blend_attachment_state);
+
+	VkPipelineDepthStencilStateCreateInfo depth_stencil_state =
+	    vkb::initializers::pipeline_depth_stencil_state_create_info(
+	        VK_TRUE,
+	        VK_TRUE,
+	        VK_COMPARE_OP_GREATER);
+
+	VkPipelineViewportStateCreateInfo viewport_state =
+	    vkb::initializers::pipeline_viewport_state_create_info(1, 1, 0);
+
+	VkPipelineMultisampleStateCreateInfo multisample_state =
+	    vkb::initializers::pipeline_multisample_state_create_info(
+	        VK_SAMPLE_COUNT_1_BIT,
+	        0);
+
+	std::vector<VkDynamicState> dynamic_state_enables = {
+	    VK_DYNAMIC_STATE_VIEWPORT,
+	    VK_DYNAMIC_STATE_SCISSOR};
+
+	VkPipelineDynamicStateCreateInfo dynamic_state =
+	    vkb::initializers::pipeline_dynamic_state_create_info(
+	        dynamic_state_enables.data(),
+	        static_cast<uint32_t>(dynamic_state_enables.size()),
+	        0);
+
+	std::array<VkPipelineShaderStageCreateInfo, 2> shader_stages;
+
+	shader_stages[0] = load_shader("subgroups_operations/base.vert", VK_SHADER_STAGE_VERTEX_BIT);
+	shader_stages[1] = load_shader("subgroups_operations/base.frag", VK_SHADER_STAGE_FRAGMENT_BIT);
+
+	const std::vector<VkVertexInputBindingDescription> vertex_input_bindings = {
+	    vkb::initializers::vertex_input_binding_description(0, sizeof(TextureQuadVertex), VK_VERTEX_INPUT_RATE_VERTEX),
+	};
+	const std::vector<VkVertexInputAttributeDescription> vertex_input_attributes = {
+	    vkb::initializers::vertex_input_attribute_description(0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(TextureQuadVertex, pos)),
+	    vkb::initializers::vertex_input_attribute_description(0, 1, VK_FORMAT_R32G32_SFLOAT, offsetof(TextureQuadVertex, uv)),
+	};
+	VkPipelineVertexInputStateCreateInfo vertex_input_state = vkb::initializers::pipeline_vertex_input_state_create_info();
+	vertex_input_state.vertexBindingDescriptionCount        = static_cast<uint32_t>(vertex_input_bindings.size());
+	vertex_input_state.pVertexBindingDescriptions           = vertex_input_bindings.data();
+	vertex_input_state.vertexAttributeDescriptionCount      = static_cast<uint32_t>(vertex_input_attributes.size());
+	vertex_input_state.pVertexAttributeDescriptions         = vertex_input_attributes.data();
+
+	VkGraphicsPipelineCreateInfo pipeline_create_info =
+	    vkb::initializers::pipeline_create_info(
+	        pipeline_layout,
+	        render_pass,
+	        0);
+
+	pipeline_create_info.pVertexInputState   = &vertex_input_state;
+	pipeline_create_info.pInputAssemblyState = &input_assembly_state;
+	pipeline_create_info.pRasterizationState = &rasterization_state;
+	pipeline_create_info.pColorBlendState    = &color_blend_state;
+	pipeline_create_info.pMultisampleState   = &multisample_state;
+	pipeline_create_info.pViewportState      = &viewport_state;
+	pipeline_create_info.pDepthStencilState  = &depth_stencil_state;
+	pipeline_create_info.pDynamicState       = &dynamic_state;
+	pipeline_create_info.stageCount          = static_cast<uint32_t>(shader_stages.size());
+	pipeline_create_info.pStages             = shader_stages.data();
+
+	VK_CHECK(vkCreateGraphicsPipelines(get_device().get_handle(), pipeline_cache, 1, &pipeline_create_info, nullptr, &texturePipeline));
+}
+
+void SubgroupsOperations::setup_descriptor_pool()
+{
+	std::vector<VkDescriptorPoolSize> pool_sizes =
+	    {
+	        vkb::initializers::descriptor_pool_size(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1),
+	        vkb::initializers::descriptor_pool_size(VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1)};
+
+	VkDescriptorPoolCreateInfo descriptor_pool_create_info =
+	    vkb::initializers::descriptor_pool_create_info(
+	        static_cast<uint32_t>(pool_sizes.size()),
+	        pool_sizes.data(),
+	        2);
+
+	VK_CHECK(vkCreateDescriptorPool(get_device().get_handle(), &descriptor_pool_create_info, nullptr, &descriptor_pool));
+}
+
+void SubgroupsOperations::setup_descriptor_set_layout()
+{
+	std::vector<VkDescriptorSetLayoutBinding> set_layout_bindings =
+	    {
+	        // Binding 0 : Vertex shader uniform buffer
+	        vkb::initializers::descriptor_set_layout_binding(
+	            VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+	            VK_SHADER_STAGE_VERTEX_BIT,
+	            0),
+	        // Binding 1 : Fragment shader image sampler
+	        vkb::initializers::descriptor_set_layout_binding(
+	            VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+	            VK_SHADER_STAGE_FRAGMENT_BIT,
+	            1)};
+
+	VkDescriptorSetLayoutCreateInfo descriptor_layout =
+	    vkb::initializers::descriptor_set_layout_create_info(
+	        set_layout_bindings.data(),
+	        static_cast<uint32_t>(set_layout_bindings.size()));
+
+	VK_CHECK(vkCreateDescriptorSetLayout(get_device().get_handle(), &descriptor_layout, nullptr, &descriptor_set_layout));
+
+	VkPipelineLayoutCreateInfo pipeline_layout_create_info =
+	    vkb::initializers::pipeline_layout_create_info(
+	        &descriptor_set_layout,
+	        1);
+
+	VK_CHECK(vkCreatePipelineLayout(get_device().get_handle(), &pipeline_layout_create_info, nullptr, &pipeline_layout));
+}
+
+void SubgroupsOperations::setup_descriptor_set()
+{
+	VkDescriptorSetAllocateInfo alloc_info =
+	    vkb::initializers::descriptor_set_allocate_info(
+	        descriptor_pool,
+	        &descriptor_set_layout,
+	        1);
+
+	VK_CHECK(vkAllocateDescriptorSets(get_device().get_handle(), &alloc_info, &descriptor_set));
+
+	VkDescriptorBufferInfo buffer_descriptor = create_descriptor(*uniform_buffer_vs);
+
+	// Setup a descriptor image info for the current texture to be used as a combined image sampler
+	VkDescriptorImageInfo image_descriptor = create_descriptor(texture);
+
+	std::vector<VkWriteDescriptorSet> write_descriptor_sets =
+	    {
+	        // Binding 0 : Vertex shader uniform buffer
+	        vkb::initializers::write_descriptor_set(
+	            descriptor_set,
+	            VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+	            0,
+	            &buffer_descriptor),
+	        // Binding 1 : Fragment shader texture sampler
+	        //	Fragment shader: layout (binding = 1) uniform sampler2D samplerColor;
+	        vkb::initializers::write_descriptor_set(
+	            descriptor_set,
+	            VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,        // The descriptor set will use a combined image sampler (sampler and image could be split)
+	            1,                                                // Shader binding point 1
+	            &image_descriptor)                                // Pointer to the descriptor image for our texture
+	    };
+
+	vkUpdateDescriptorSets(get_device().get_handle(), static_cast<uint32_t>(write_descriptor_sets.size()), write_descriptor_sets.data(), 0, NULL);
+}
+
+void SubgroupsOperations::generate_quad()
+{
+	std::vector<TextureQuadVertex> vertices =
+	    {
+	        {{1.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},
+	        {{-1.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
+	        {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f}},
+	        {{1.0f, -1.0f, 0.0f}, {1.0f, 0.0f}}};
+
+	std::vector<uint32_t> indices = {0, 1, 2, 2, 3, 0};
+	index_count                   = static_cast<uint32_t>(indices.size());
+
+	auto vertex_buffer_size = vkb::to_u32(vertices.size() * sizeof(TextureQuadVertex));
+	auto index_buffer_size  = vkb::to_u32(indices.size() * sizeof(uint32_t));
+
+	vertex_buffer = std::make_unique<vkb::core::Buffer>(get_device(),
+	                                                    vertex_buffer_size,
+	                                                    VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+	                                                    VMA_MEMORY_USAGE_CPU_TO_GPU);
+	vertex_buffer->update(vertices.data(), vertex_buffer_size);
+
+	index_buffer = std::make_unique<vkb::core::Buffer>(get_device(),
+	                                                   index_buffer_size,
+	                                                   VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+	                                                   VMA_MEMORY_USAGE_CPU_TO_GPU);
+
+	index_buffer->update(indices.data(), index_buffer_size);
+}
+
+void SubgroupsOperations::load_assets()
+{
+	texture = load_texture("textures/vulkan_logo_full.ktx", vkb::sg::Image::Color);        // TODO: find better image
+}
+
+void SubgroupsOperations::create_command_buffers()
+{
+	draw_cmd_buffers.resize(render_context->get_render_frames().size());
+
+	VkCommandBufferAllocateInfo allocate_info =
+	    vkb::initializers::command_buffer_allocate_info(
+	        cmd_pool,
+	        VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+	        static_cast<uint32_t>(draw_cmd_buffers.size()));
+
+	VK_CHECK(vkAllocateCommandBuffers(device->get_handle(), &allocate_info, draw_cmd_buffers.data()));
+}
+
+void SubgroupsOperations::build_command_buffers()
+{
+	VkCommandBufferBeginInfo command_buffer_begin_info = vkb::initializers::command_buffer_begin_info();
+
+	std::array<VkClearValue, 2> clear_values;
+	clear_values[0].color        = {{0.0f, 0.0f, 0.0f, 0.0f}};
+	clear_values[1].depthStencil = {0.0f, 0};
+
+	VkRenderPassBeginInfo render_pass_begin_info    = vkb::initializers::render_pass_begin_info();
+	render_pass_begin_info.renderPass               = render_pass;
+	render_pass_begin_info.renderArea.extent.width  = width;
+	render_pass_begin_info.renderArea.extent.height = height;
+	render_pass_begin_info.clearValueCount          = static_cast<uint32_t>(clear_values.size());
+	render_pass_begin_info.pClearValues             = clear_values.data();
+
+	for (int32_t i = 0; i < draw_cmd_buffers.size(); ++i)
+	{
+		render_pass_begin_info.framebuffer = framebuffers[i];
+
+		VK_CHECK(vkBeginCommandBuffer(draw_cmd_buffers[i], &command_buffer_begin_info));
+
+		vkCmdBeginRenderPass(draw_cmd_buffers[i], &render_pass_begin_info, VK_SUBPASS_CONTENTS_INLINE);
+
+		VkViewport viewport = vkb::initializers::viewport(static_cast<float>(width), static_cast<float>(height), 0.0f, 1.0f);
+		vkCmdSetViewport(draw_cmd_buffers[i], 0, 1, &viewport);
+
+		VkRect2D scissor = vkb::initializers::rect2D(width, height, 0, 0);
+		vkCmdSetScissor(draw_cmd_buffers[i], 0, 1, &scissor);
+
+		// draw texture
+		vkCmdBindDescriptorSets(draw_cmd_buffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline_layout, 0, 1, &descriptor_set, 0, NULL);
+		vkCmdBindPipeline(draw_cmd_buffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, texturePipeline);
+
+		VkDeviceSize offsets[1] = {0};
+		vkCmdBindVertexBuffers(draw_cmd_buffers[i], 0, 1, vertex_buffer->get(), offsets);
+		vkCmdBindIndexBuffer(draw_cmd_buffers[i], index_buffer->get_handle(), 0, VK_INDEX_TYPE_UINT32);
+
+		vkCmdDrawIndexed(draw_cmd_buffers[i], index_count, 1, 0, 0, 0);
+		draw_ui(draw_cmd_buffers[i]);
+
+		vkCmdEndRenderPass(draw_cmd_buffers[i]);
+
+		VK_CHECK(vkEndCommandBuffer(draw_cmd_buffers[i]));
+	}
+}
+
+void SubgroupsOperations::draw()
+{
+	ApiVulkanSample::prepare_frame();
+	submit_info.commandBufferCount = 1;
+	submit_info.pCommandBuffers    = &draw_cmd_buffers[current_buffer];
+	VK_CHECK(vkQueueSubmit(queue, 1, &submit_info, VK_NULL_HANDLE));
+	ApiVulkanSample::submit_frame();
+}
+
+void SubgroupsOperations::on_update_ui_overlay(vkb::Drawer &drawer)
+{
+	if (drawer.header("Settings"))
+	{
+		if (drawer.combo_box("Filters", &guiSettings.selected_filtr, guiSettings.init_filters_name()))
+		{
+			update_uniform_buffers();
+		}
+	}
+}
+
+bool SubgroupsOperations::resize(const uint32_t width, const uint32_t height)
+{
+	if (!ApiVulkanSample::resize(width, height))
+		return false;
+	build_command_buffers();
+	update_uniform_buffers();
+	return true;
+}
+
+void SubgroupsOperations::render(float delta_time)
+{
+	if (!prepared)
+	{
+		return;
+	}
+	draw();
+	if (camera.updated)
+	{
+		update_uniform_buffers();
+	}
+}
+
+std::unique_ptr<vkb::VulkanSample> create_subgroups_operations()
+{
+	return std::make_unique<SubgroupsOperations>();
+}
---
+++ samples/extensions/subgroups_operations/subgroups_operations.h
@@ -0,0 +1,84 @@
+/* Copyright (c) 2023, Mobica Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "api_vulkan_sample.h"
+
+struct TextureQuadVertex
+{
+	glm::vec3 pos;
+	glm::vec2 uv;
+};
+
+class SubgroupsOperations : public ApiVulkanSample
+{
+  public:
+	SubgroupsOperations();
+	~SubgroupsOperations();
+
+	bool prepare(vkb::Platform &platform) override;
+	void request_gpu_features(vkb::PhysicalDevice &gpu) override;
+	void build_command_buffers() override;
+	void render(float delta_time) override;
+	bool resize(const uint32_t width, const uint32_t height) override;
+	void on_update_ui_overlay(vkb::Drawer &drawer) override;
+
+	void draw();
+	void load_assets();
+
+	void generate_quad();
+	void setup_descriptor_pool();
+	void setup_descriptor_set_layout();
+	void setup_descriptor_set();
+	void setup_pipelines();
+	void prepare_uniform_buffers();
+	void update_uniform_buffers();
+	void create_command_buffers();
+
+	struct GuiSettings
+	{
+		int32_t                         selected_filtr = 0;
+		static std::vector<std::string> init_filters_name()
+		{
+			std::vector<std::string> filtrs = {
+			    {"Blur"}, {"Sharpen"}, {"Edge detection vertical, horizontal, and diagonal"}, {"Canny edge"}};
+			return filtrs;
+		}
+	} guiSettings;
+
+	Texture                            texture;
+	std::unique_ptr<vkb::core::Buffer> vertex_buffer;
+	std::unique_ptr<vkb::core::Buffer> index_buffer;
+	uint32_t                           index_count;
+
+	std::unique_ptr<vkb::core::Buffer> uniform_buffer_vs;
+
+	struct
+	{
+		glm::mat4 projection;
+		glm::mat4 view;
+		glm::mat4 model;
+	} ubo_vs;
+
+	VkPipeline            texturePipeline;
+	VkPipelineLayout      pipeline_layout;
+	VkDescriptorSet       descriptor_set;
+	VkDescriptorSetLayout descriptor_set_layout;
+};
+
+std::unique_ptr<vkb::VulkanSample> create_subgroups_operations();
